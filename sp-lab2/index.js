ba2c8f05eb4501843dc314ac4f87081a2d2ab56c
Den <denisanatoliyovich@gmail.com>
2019-09-28 18:45:06 +0300

    Created a graph class. In its constructor we declare and fill with GraphNodes our 'nodes' array. Of course, were took into consideration specific  switches between nodes. Also we create a 'currentNode' variable and immedeately assign its value as the first element of the nodes array.

--- sp-lab2/index.js
+++ sp-lab2/index.js
@@ -17,3 +17,26 @@ class GraphNode {
   }
   //TODO dlm cfr ltr
 }
+
+class Graph {
+  constructor() {
+    this.nodes = [];
+    for (let i = 2; i < 10; i++) {
+      if (i === 3) {
+        this.nodes.push(new GraphNode(i, 1, 1, 4));
+      } else if (i === 5) {
+        this.nodes.push(new GraphNode(i, 1, 2, 1));
+      } else if (i === 8) {
+        this.nodes.push(new GraphNode(i, -6, 1, 1));
+      } else if (i === 9) {
+        this.nodes.push(new GraphNode(i, 0, 0, 0));
+      } else {
+        this.nodes.push(new GraphNode(i));
+      }
+    }
+    this.currentNode = this.nodes[0];
+    console.log(this.nodes);
+    console.log(this.currentNode);
+  }
+}
+const graph = new Graph();
