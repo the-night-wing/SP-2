8a73f9aae28f9ad941dfa35cc539b0431657bd3f
Den <denisanatoliyovich@gmail.com>
2019-10-11 22:31:39 +0300

    Brand new version of third sp lab. Completely changed my approach to the task. Now everything hangs on RegExp. Currently the app identifies and extracts invalid and valid variables,  and reserved words.

--- sp-lab3/sp-lab3.js
+++ sp-lab3/sp-lab3.js
@@ -1,24 +1,61 @@
-const str = "switch (c) {case 1:console.trace(c); break; }";
-
+const expression = "case c begin 0: b:=2*a[n]; else: b:=d end 2asbddas";
+const invalidVariables = [];
+const words = `and array begin case const div do downto else end file for function goto if in label mod nil not of or packed procedure program record repeat set then to type until var while with integer char real string boolean array file set record enumerated subrange`;
+const reservedWordsList = words.split(" ");
+const validVariables = [];
 const reservedWords = [];
-// reservedWords.findIndex(val);
 
-class A {
-  run(string) {
-    let withoutSemiColumn = string.split(";");
-    let withoutSpaces = [];
-    withoutSemiColumn.forEach((e, i) => {
-      withoutSpaces[i] = e.trim();
-    });
-    withoutSpaces.forEach(el => {
-      if (el === "{") console.log("opening bracket");
-      if (el === "}") console.log("closing bracket");
-    });
-    console.log(withoutSpaces);
-    // console.log(sp);
+const analyse = expression => {
+  let exprCopy = expression;
+  ///TODO Get rid of invalid identifiers;
+  //TODO extract symbols
+  //TODO locate valid indentifiers;
+  //TODO find reserved words
+  const regx = /\d+[A-Za-z\,\.\;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]+/; //! Locates a word starting with a digit
+  const regx1 = /[\,\.\;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]+[A-Za-z\,\.\;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]*/; //!Locates a word that starts with an invalid symbol
+  const regx2 = /[A-Za-z]+[0-9\,\.\d;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]+[0-9A-Za-z\,\.\;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]*/; //! Locates a word starting with a letter and containing invalid symbols
+  const invRegex = [
+    /\d+[A-Za-z]+/
+    // /[\,\.\;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]+[A-Za-z\,\.\;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]*/,
+    // /[A-Za-z]+[0-9\,\.\d;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]+[0-9A-Za-z\,\.\;\:\{\}\(\)\[\]\+\+=\:=\-\-=\<\>\*\/]*/
+  ];
+  const symbolsRegex = / [\,\.\;\:\{\}\(\)\[\]\+=\:=\-=\-\-\<\>\*\/]+ /;
+  const varRegex = /[A-Za-z]+[0-9_]*[A-Za*z]*/;
+  for (let i = 0; i < invRegex.length; i++) {
+    let lastMatch = exprCopy.match(invRegex[i]);
+    console.log(lastMatch[0]);
+    if (lastMatch !== null) {
+      invalidVariables.push(lastMatch[0]);
+      exprCopy = expression.replace(lastMatch[0], " ");
+    }
   }
-}
+  //   console.log(invalidVariables);
 
-const a = new A();
+  let possVarMatch = exprCopy.match(varRegex);
+  console.log(possVarMatch[0]);
+  while (possVarMatch !== null) {
+    let flagReserved = false;
+    for (const reswordIndex in reservedWordsList) {
+      if (possVarMatch[0] === reservedWordsList[reswordIndex]) {
+        console.log("reserved");
+        reservedWords.push(possVarMatch[0]);
+        exprCopy = exprCopy.replace(possVarMatch[0], " ");
+        possVarMatch = exprCopy.match(varRegex);
+        flagReserved = true;
+        break;
+      }
+    }
+    if (!flagReserved) {
+      validVariables.push(possVarMatch[0]);
+      exprCopy = exprCopy.replace(possVarMatch[0], " ");
+      possVarMatch = exprCopy.match(varRegex);
+    }
+  }
 
-a.run(str);
+  return exprCopy;
+};
+
+console.log(analyse(expression));
+console.log(validVariables);
+console.log(invalidVariables);
+console.log(reservedWords);
